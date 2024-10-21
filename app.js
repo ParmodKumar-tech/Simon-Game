2e06060b705dde5cf076c0cb314a93705c41f99b
ParmodKumar-tech <parmod1feb2002@gmail.com>
2024-10-21 14:04:03 +0530

    added

--- app.js
+++ app.js
@@ -1,90 +1,95 @@
 
 let gameSeq=[];
 let userSeq=[];
-let btnsColor=["brown","skyblue","yellow","darkpurple"];
+
+let btnsColor=["orange","purple","cyan","crimson"]
 
 let startGame=false;
 let level=0;
-let highScore=0;
+
+let h2=document.querySelector("h2");
+let allBtns=document.querySelectorAll(".btn");
+let body=document.querySelector("body");
+let startBtn=document.querySelector(".startBtn");
 
 
-let titleOfGame=document.querySelector("h1");
-let levelLabel=document.querySelector("h3");
-let highScoreLabel=document.querySelector("h2");
-let btns=document.querySelectorAll(".btn");
-let startGameBtn=document.querySelector(".startGameBtn");
 
-
-document.addEventListener("click",startingPointOfGame);
-document.addEventListener("keypress",startingPointOfGame);
-
-let startingPointOfGame=()=>{
-    if(!startGame){
+function BeginGame(){
+    if(startGame == false){
         startGame=true;
+        body.style.backgroundColor="white"
         console.log("game start");
         levelUp();
-        
     }
-}
-
-
-
-function buttonFlash(btn){
-    btn.classList.add("btnflash");
-    setTimeout(function(){
-        btn.classList.remove("btnflash")
-    },200);
+   
 
 }
+
+startBtn.addEventListener("click",BeginGame)
+
+document.addEventListener("keypress",BeginGame)
+
 
 function levelUp(){
     userSeq=[];
     level++;
-    levelLabel.innerText=`Level ${level}`;
-    let rand=Math.floor(Math.random()*4);
-    let randColor=btnsColor[rand];
-    let randBtn=document.querySelector(`.${randColor}`)
-    
-    gameSeq.push(randColor)
-    console.log(gameSeq)
-    
-    buttonFlash(randBtn);
+    h2.innerText=`Level ${level}`;
+
+    let randIdx=Math.floor(Math.random()*4);
+    let colorBasedOnIdx=btnsColor[randIdx];
+    let btn=document.querySelector(`.${colorBasedOnIdx}`);
+    gameSeq.push(colorBasedOnIdx);
+
+    btnFlashByGame(btn);
 }
 
-function userFlashBtn(btn){
-    btn.classList.add("userflash");
+
+function btnFlashByGame(btn){
+    btn.classList.add("gameFlash");
     setTimeout(function(){
-        btn.classList.remove("userflash")
+        btn.classList.remove("gameFlash");
     },200);
+
 }
 
+function btnFlashByUser(btn){
+    btn.classList.add("userFlash");
+    setTimeout(function(){
+        btn.classList.remove("userFlash");
+    },250);
+}
 
-function checkAns(idx){
-    // console.log(`current level ${level}`)
+function btnPress(){
+    let clickedBtn=this;
+    btnFlashByUser(this);
    
-    if(userSeq[idx]===gameSeq[idx]){
-        if(userSeq.length==gameSeq.length){
-            // console.log("same value");
-            setTimeout(levelUp,300)
-            
+    let colorByClickedBtn=clickedBtn.getAttribute("id");
+    userSeq.push(colorByClickedBtn);
+    checkSeq(userSeq.length-1);
+}
+
+function checkSeq(idx){
+
+    if(gameSeq[idx]===userSeq[idx]){
+        if(gameSeq.length===userSeq.length){
+            setTimeout(()=>{
+                levelUp();
+
+            },700);
         }
     }
     else{
-        levelLabel.innerHTML=`Game Over! Your Score: ${level} <br> Press any key to start`;
-        // document.querySelector("body").style.backgroundColor="red";
-        
-        setTimeout(function(){
-            document.querySelector("body").style.backgroundColor="white";
-        },150)
-
-        if(highScore<level){
-            highScore=level;
-        }
-        highScoreLabel.innerText=`High Score: ${highScore}`;
+        h2.innerHTML=`Game Over!, Your Score : ${level} <br>press or click to restart!`;
+        body.style.backgroundColor="#ced8ff"
         resetGame();
-
     }
 }
+
+for(btn of allBtns){
+    btn.addEventListener("click",btnPress)
+        
+    }
+
 
 function resetGame(){
     startGame=false;
@@ -93,18 +98,3 @@ function resetGame(){
     level=0;
 }
 
-function btnPress(){
-    let btn=this;
-  
-    userFlashBtn(btn);
-    let userColor=btn.getAttribute("id");
-    userSeq.push(userColor);
-    
-   
-    checkAns(userSeq.length-1);
-
-}
-
-for(buttons of btns){
-    buttons.addEventListener("click",btnPress)
-}
